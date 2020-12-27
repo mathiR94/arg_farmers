355975687a4363323c46f3d93ed5303542d432cc
Mathías Ribeiro <m.ribeiro@um.edu.uy>
2020-12-27 16:49:50 -0300

    create getting_all_borders

---
+++ scripts/create_dataset/maps/boundaries/getting_all_borders.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import geopandas as gpd
+import pandas as pd
+import matplotlib as mt
+import descartes
+import fiona
+
+from shapely.geometry import mapping
+from shapely.wkt import loads
+
+
+
+limits  = gpd.read_file("C:/Users/ribei/Dropbox/farmers_arg/data/intermediate_data/maps/raw_maps/arg_limits/linea_de_limite_070111.shp")
+
+#here we construct the borders as unique lines objects#
+
+
+borders = set(list(limits["gna"]))
+
+#getting the crs of each figure
+limits.crs
+
+#setting the schema for each figure
+schema = {
+    'geometry': 'LineString',
+    'properties': {'id': 'int'},
+}
+             
+for i in borders:
+    
+    j = int(1)
+    border = limits[limits["gna"] == str(i)]
+    border_u = border.unary_union
+    
+    file_name = str( "/" + str(i).lower().replace("-", "_").replace(" ", "") + ".shp")
+    path = "C:/Users/ribei/Dropbox/farmers_arg/data/intermediate_data/maps/raw_maps/arg_limits/inter_prov/" + file_name 
+    
+    with fiona.open(path, 'w', 'ESRI Shapefile', schema = schema, crs = fiona.crs.from_epsg(4326)) as c:
+            c.write({
+                'geometry': mapping(border_u),
+                'properties': {'id': int(j)},
+            })
+    j = j + 1
+
+
+    
+    
+    
+    
+
+
+
